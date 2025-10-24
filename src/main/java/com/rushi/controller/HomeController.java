30e13a2a7a36d6e61556b0c0797e5f6602c7ecf0
sachinthitame123 <sachinthitame7350@gmail.com>
2025-10-24 13:03:00 +0530

    updated code java

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -91,6 +91,6 @@ public class HomeController {
     }
 
     private List<String> getCourses() {
-        return Arrays.asList("DevOps", "AWS", "python","ruby");
+        return Arrays.asList("DevOps", "AWS", "python","ruby","sachin");
     }
 }
